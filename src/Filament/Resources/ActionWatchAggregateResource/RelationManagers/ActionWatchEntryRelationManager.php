06e9eb081ace23938694991ac37de1b66c4bcc0f
atendwa <atendwa@gmail.com>
2025-06-12 12:03:48 +0300

    fix(filament): updates attributes used

--- src/Filament/Resources/ActionWatchAggregateResource/RelationManagers/ActionWatchEntryRelationManager.php
+++ src/Filament/Resources/ActionWatchAggregateResource/RelationManagers/ActionWatchEntryRelationManager.php
@@ -28,7 +28,7 @@ class ActionWatchEntryRelationManager extends RelationManager
         self::$customTable = $table->columns([
             column('user.' . $attribute)->label("User's " . $attribute),
             column('formatted_metrics')->label('Metrics')->badge()
-                ->color(fn ($record): string => FormatMetrics::colour($record->getAttribute('formatted_metrics'), $record->getAttribute('class'))),
+                ->color(fn ($record): string => FormatMetrics::colour($record->formatted_metrics, $record->getAttribute('class'))),
             column('formatted_constraints')->label('Constraints')->badge(),
         ]);
 
